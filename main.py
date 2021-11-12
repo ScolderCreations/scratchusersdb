97d16d15bb2e79967161907087950acdc83ff82a
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-12 08:30:05 -0800

    formatted

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
-from requests_html import HTMLSession
 from random import randint
+
+from requests_html import HTMLSession
+
 session = HTMLSession()
 usersfound = set(())
 specildeal = "https://scratch.mit.edu/explore/studios/all/"
 filef = open("users.txt", "r")
 filecontent = filef.read()
 currentpage = session.get('http://scratch.mit.edu')
+
+
 def loadpage():
-  print(currentpage.url)
-  try:
-    currentpage.html.render()
-  except:
-    print("page render failed")
-  links = currentpage.html.absolute_links
-  
-  for link in links:
-    if "/scratch.mit.edu/users/" in link and not "/studios/" in link and not "/studios" in link and not "/favorites/" in link and not "/followers/" in link and not "/following/" in link and not "/projects/" in link and not "#comm" in link and not link in filecontent:
-      usersfound.add(link)
-  print("progressing")
-  filex = open("users.txt", "r")
-  filecont = filex.read()
-  filex.close()
-  filex = open("users.txt", "a")
-  
-  for link in usersfound:
-    if not link in filecont:
-      filex.write(link + "\n")
-    
-  filex.close()
-  return usersfound
+    print(currentpage.url)
+    try:
+        currentpage.html.render()
+    except:
+        print("page render failed")
+    links = currentpage.html.absolute_links
+
+    for link in links:
+        if "/scratch.mit.edu/users/" in link and not "/studios/" in link and not "/studios" in link and not "/favorites/" in link and not "/followers/" in link and not "/following/" in link and not "/projects/" in link and not "#comm" in link and not link in filecontent:
+            usersfound.add(link)
+    print("progressing")
+    filex = open("users.txt", "r")
+    filecont = filex.read()
+    filex.close()
+    filex = open("users.txt", "a")
+
+    for link in usersfound:
+        if not link in filecont:
+            filex.write(link + "\n")
+
+    filex.close()
+    return usersfound
+
+
 filex = open("users.txt", "r")
 
 loadpage()
@@ -53,7 +59,7 @@ currentpage = session.get(specildeal)
 loadpage()
 
 if specildeal[len(specildeal) - 1] == "/":
-  currentpage = session.get(specildeal + 'followers/')
+    currentpage = session.get(specildeal + 'followers/')
 else:
-  currentpage = session.get(specildeal + '/followers/')
+    currentpage = session.get(specildeal + '/followers/')
 loadpage()
