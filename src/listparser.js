cc8ad5dd6904ac554f294082e2a743d5e7360aa7
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-15 18:30:37 -0800

    Update listparser.js

--- src/listparser.js
+++ src/listparser.js
@@ -1,1 +1,10 @@
+function lpCreateList1() {
+  fetch('https://scoldercreations.github.io/scratchusersdb/users.txt')
+  .then(response => response.text())
+  .then(data => var userstxt = data);
 
+  userstxt = userstxt.split("\n");
+  return userstxt
+}
+
+var scratchusersarray = lpCreateList1();
