996ea2c56981cf80d8e30a85984dcaef8f3a8c78
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-17 13:39:05 -0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -3,21 +3,13 @@ y = time.gmtime()
 readf = open('users.txt', 'r')
 writef = open('users.txt', 'w')
 print(y)
-filelines = readf.read().split('\n')
-nfilelines = set(())
-newlines = []
-for line in filelines:
-    f = line.replace("//scratch.mit.edu/users/", "")
-    f = f.replace("https:", "")
-    f = f.replace("http:", "")
-    f = f.replace("/", "")
-    if not f in newlines:
-        newlines += f
-print(newlines)
-lts = ""
-for line in newlines:
-    lts = lts + line
-writef.write(lts)
+filelines = readf.read()
+f = filelines.replace("//scratch.mit.edu/users/", "")
+f = f.replace("https:", "")
+f = f.replace("http:", "")
+f = f.replace("/", "")
+print(len(f))
+writef.write(f)
 writef.close()
 readf.close()
 print(time.gmtime())
