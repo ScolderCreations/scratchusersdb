84626a7579553bb8428a5a251957a198d3040803
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-17 09:30:19 -0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -4,9 +4,12 @@ readf = open('users.txt', 'r')
 writef = open('users.txt', 'w')
 
 filelines = readf.readlines()
-
+nfilelines = set(())
 for line in filelines:
     f = line.replace("//scratch.mit.edu/users/", "")
     f = f.replace("https:", "")
     f = f.replace("http:", "")
     f = f.replace("/", "")
+    nfilelines.add(f)
+
+writef.write(nfilelines)
