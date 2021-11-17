43fde8abd1a2679cb6a0805f2b5374a24bd42728
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-16 17:38:20 -0800

    a

--- filter.py
+++ filter.py
@@ -5,3 +5,8 @@ writef = open('users.txt', 'w')
 
 filelines = readf.readlines()
 
+for line in filelines:
+    f = line.replace("//scratch.mit.edu/users/", "")
+    f = f.replace("https:", "")
+    f = f.replace("http:", "")
+    f = f.replace("/", "")
