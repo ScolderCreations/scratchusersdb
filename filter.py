b37a0c7a36914657ec2229d0667ac6fe8e825e37
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-17 17:46:29 -0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -6,7 +6,7 @@ for line in readf:
         if word in line:
             line = line.replace(word,'')
     lst.append(line)
-f.close()
+readf.close()
 f = open('./users.txt','w')
 for line in lst:
     f.write(line)
