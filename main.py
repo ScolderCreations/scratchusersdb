4891499917b3ec08014b38d07df6dbcfab80fe9d
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-14 12:00:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ filef = open("users.txt", "r")
 filecontent = filef.read()
 currentpage = session.get('http://scratch.mit.edu')
 
-if len(filef.readlines()) > 12345:
+if len(filef.readlines()) > 100000:
     raise Exception("File too large to edit safely")
 def loadpage():
     print(currentpage.url)
@@ -26,11 +26,11 @@ def loadpage():
     filex = open("users.txt", "r")
     filecont = filex.read()
     filex.close()
-    filex = open("users.txt", "a")
+    filex = open("users.txt", "w")
 
     for link in usersfound:
         if not link in filecont and not link + "/" in filecont and not link in filecont:
-            filex.write(link + "\n")
+            filex.append(link + "\n")
     
     usersfound.clear()
     filex.close()
@@ -53,14 +53,15 @@ loadpage()
 
 filex = open("users.txt", "r")
 flex = filex.readlines()
+filex.close()
 for i in range(1,30):
     specildeal = flex[len(flex) - randint(5, 1000)]
     specildeal = specildeal.replace("\n", "")
-    specildeal = f'https://scratch.mit.edu/users/{specildeal}/' 
+    specildeal = f'https://scratch.mit.edu/users/{specildeal}' 
     currentpage = session.get(specildeal)
 
     loadpage()
-    time.sleep(1)
+    time.sleep(0.5)
     if specildeal[len(specildeal) - 1] == "/":
         currentpage = session.get(specildeal + 'followers/')
     else:
